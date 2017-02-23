b404e59edeeb1351e386a9f42234a8aeeda036d5
Leo Febey <overclockedamiga@gmail.com>
2017-02-23 17:18:54 +1100

    Testing platform detection, startin to implement oauth login..

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -14,6 +14,7 @@ import { GoogleMap,
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var window:any;
 
 @Component({
   selector: 'page-home',
@@ -9,12 +11,94 @@ import { NavController } from 'ionic-angular';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	apiURL:string = 'https://accounts.tidyhq.com/oauth/authorize';
+	debugtext:string;
 
-  }
 
-  ionViewDidLoad() {
-  }
+	constructor(public navCtrl: NavController, private platform:Platform) {
+		this.platform = platform;
+	}
+
+	ionViewDidLoad() {
+		this.debugtext = "HELLO!!";
+	}
+
+	public login(){
+		console.log("Click!!");
+		console.log(this.platform.platforms());
+
+		var ima:boolean = this.checkIfMobilePlatform();
+		
+		if(ima == true){
+			this.debugtext = "web app";
+		} else {
+			this.debugtext = "mobile app";
+		}
+
+		/*
+		
+		this.platform.ready().then(() => {
+	        this.tidyhqLogin().then(success => {
+	            alert(success.access_token);
+	        }, (error) => {
+	            alert(error);
+	        });
+    	}); */
+	}
+
+	private checkIfMobilePlatform():boolean{
+		let isWebApp:boolean = false;
+		if (this.platform.platforms().includes('ios') ||
+			this.platform.platforms().includes('android')){
+			isWebApp = true;
+		}
+		
+		return !isWebApp;
+	}
+
+	public tidyhqLogin(): Promise<any>{
+		var pt = this.platform;
+		var apiURL = this.apiURL;
+		var debugtext = this.debugtext;
+		var that = this;
+		return new Promise(function(resolve, reject){
+			
+
+			var browserRef:any;
+			apiURL += "?client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&response_type=code";
+			
+			var isWebApp:boolean = that.checkIfMobilePlatform();
+
+			if(isWebApp){
+				browserRef = window.open(apiURL);
+			} else {
+				browserRef = window.cordova.InAppBrowser.open(apiURL);
+			}
+
+			that.debugtext = "update?";
+
+			browserRef.addEventListener("loadstart", (event) => {
+            if ((event.url).indexOf("http://localhost/callback") === 0) {
+                browserRef.removeEventListener("exit", (event) => {});
+                browserRef.close();
+                var responseParameters = ((event.url).split("#")[1]).split("&");
+                var parsedResponse = {};
+                for (var i = 0; i < responseParameters.length; i++) {
+                    parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+                }
+                if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
+                    resolve(parsedResponse);
+                } else {
+                    reject("Problem authenticating with TidyHQ");
+                }
+            }
+        });
+        browserRef.addEventListener("exit", function(event) {
+            reject("The Facebook sign in flow was canceled");
+        });
+			
+		})
+	}
 
 
 }
