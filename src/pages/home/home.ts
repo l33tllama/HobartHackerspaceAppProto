d32e959ae0dc8093af426ea54a50809051982d27
Leo Febey <overclockedamiga@gmail.com>
2017-02-28 16:26:09 +1100

    Successfully getting data from TidyHQgit diff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,55 +2,90 @@ import { Component } from '@angular/core';
 
 import { NavController, Platform } from 'ionic-angular';
 
-import { TestProvider } from '../../providers/test-provider';
+import { ConfigProvider } from '../../providers/config-provider';
+
+import { TidyHQAPIProvider } from '../../providers/tidyhqapi-provider';
 
 declare var window:any;
 
 @Component(
 {  selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [TestProvider]
+  providers: [ConfigProvider, TidyHQAPIProvider]
 })
 
 export class HomePage {
 
 	apiURL:string = 'https://accounts.tidyhq.com/oauth/authorize';
 	debugtext:string;
+	configLoaded:boolean = false;
+
+	//TODO: move to temp variables..
+	client_id:string;
+	client_secret:string;
 
 	constructor(public navCtrl: NavController, 
-		private platform:Platform, private testProvider:TestProvider) {
+		private platform:Platform, private config:ConfigProvider,
+		private tidyhq:TidyHQAPIProvider) {
 		this.platform = platform;
-		this.testProvider = testProvider;
+		this.tidyhq = tidyhq;
+		this.config = config;
 	}
 
 	ionViewDidLoad() {
+		alert("LOADED!");
 		this.debugtext = "HELLO!!";
-		this.testProvider.load().then((res) =>{
-				console.log(res);
+		var configLoaded:boolean = false;
+		var that = this;
+
+		this.config.load().then((res) => {
+			if(res.hasOwnProperty('client_id') &&
+				res.hasOwnProperty('client_secret')){
+					console.log("Client ID: " + res.client_id + 
+						" client secret: " + res.client_secret);
+					that.client_id = res.client_id;
+					that.client_secret = res.client_secret;
+					that.configLoaded = true;
+				}
 			}
 		);
+
+		var query:string = window.location.hash;
+		var codeIndex:number;
+		codeIndex = query.indexOf("#access_token=");
+
+		if(codeIndex == 0){
+			console.log("API key found on load");
+			var apikey:string = query.substring(14);
+			alert(query);
+			this.tidyhq.setAPIKey(apikey);
+			this.tidyhq.getContacts(function(data){
+				console.log(data);
+				//alert(data);
+			});
+		} else {
+			console.log("No access token? " + query);
+		}
+
+		console.log(query);
+
+		
 	}
 
+	// called when button is clicked/tapped
 	public login(){
-
 		console.log("Click!!");
-		console.log(this.platform.platforms());
-
-		var ima:boolean = this.checkIfMobilePlatform();
-		
-		if(ima == true){
-			this.debugtext = "web app";
-		} else {
-			this.debugtext = "mobile app";
-		}
-		
 		this.platform.ready().then(() => {
-	        this.tidyhqLogin().then(success => {
-	            alert(success.access_token);
-	        }, (error) => {
-	            alert(error);
-	        });
-    	});
+			console.log("Connecting to tidyhq..");
+			this.tidyhq.connectToAPI(this.client_id, this.client_secret, "iframe-container").then(
+				success => function(val){
+					console.log("Success?");
+					console.log(val);
+				}, error => function (err) {
+					console.log("Error!");
+					console.log(err);
+				})
+		});
 	}
 
 	private checkIfMobilePlatform():boolean{
@@ -63,7 +98,7 @@ export class HomePage {
 		return !isWebApp;
 	}
 
-	public tidyhqLogin(): Promise<any>{
+	public tidyhqLogin(): Promise<any> {
 		var pt = this.platform;
 		var apiURL = this.apiURL;
 		var debugtext = this.debugtext;
@@ -80,33 +115,30 @@ export class HomePage {
 				browserRef = window.cordova.InAppBrowser.open(apiURL);
 			} else {
 				browserRef = window.open(apiURL);
-				
 			}
 
 			that.debugtext = "update?";
 
 			browserRef.addEventListener("loadstart", (event) => {
-            if ((event.url).indexOf("http://localhost/callback") === 0) {
-                browserRef.removeEventListener("exit", (event) => {});
-                browserRef.close();
-                var responseParameters = ((event.url).split("#")[1]).split("&");
-                var parsedResponse = {};
-                for (var i = 0; i < responseParameters.length; i++) {
-                    parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
-                }
-                if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
-                    resolve(parsedResponse);
-                } else {
-                    reject("Problem authenticating with TidyHQ");
-                }
-            }
-        });
-        browserRef.addEventListener("exit", function(event) {
-            reject("The Facebook sign in flow was canceled");
-        });
-			
+	            if ((event.url).indexOf("http://localhost/callback") === 0) {
+	                browserRef.removeEventListener("exit", (event) => {});
+	                browserRef.close();
+	                var responseParameters = ((event.url).split("#")[1]).split("&");
+	                var parsedResponse = {};
+	                for (var i = 0; i < responseParameters.length; i++) {
+	                    parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+	                }
+	                if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
+	                    resolve(parsedResponse);
+	                } else {
+	                    reject("Problem authenticating with TidyHQ");
+	                }
+            	}
+       		});
+
+	        browserRef.addEventListener("exit", function(event) {
+	            reject("The Facebook sign in flow was canceled");
+	        });
 		})
 	}
-
-
 }
---
+++ src/providers/tidyhqapi-provider.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+declare var window:any;
+
+@Injectable()
+export class TidyHQAPIProvider {
+
+	data:any;
+	access_token:string;
+	auth_login_url:string;
+
+	constructor(private http:Http, private platform:Platform){
+		this.http = http;
+		this.data = null;
+		this.access_token = null;
+		this.auth_login_url = 'https://accounts.tidyhq.com/oauth/authorize';
+	}
+
+	private checkIfMobilePlatform():boolean{
+		let isWebApp:boolean = false;
+		if (this.platform.platforms().includes('ios') ||
+			this.platform.platforms().includes('android')){
+			isWebApp = true;
+		}
+		
+		return !isWebApp;
+	}
+
+	public setAPIKey(apikey:string){
+		this.access_token = apikey;
+	}
+
+	public connectToAPI(client_id:string, client_secret:string, iframe_id:string):Promise<any>{
+		var that = this;
+
+		return new Promise(function(resolve, reject) {
+			console.log("Returing promise??");
+			
+			if(!client_id && !client_secret){
+				reject("No clientid and/or client secret");
+			}
+
+			var isMobApp = that.checkIfMobilePlatform();
+			var apiURL:string = that.auth_login_url + "?client_id=" + 
+				client_id + "&redirect_uri=http://localhost:8100&response_type=token"
+
+			if(isMobApp) {
+
+				var browserRef = window.cordova.InAppBrowser.open(apiURL, "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+				browserRef.addEventListener("load", function(event) {
+
+					console.log("Loading..");
+
+					if ((event.url).indexOf("https://tidyhq.com") === 0) {
+		                browserRef.removeEventListener("exit", (event) => {});
+		                browserRef.close();
+
+		                var responseParameters = ((event.url).split("#")[1]).split("&");
+		                var parsedResponse = {};
+		                
+		                for (var i = 0; i < responseParameters.length; i++) {
+		                    parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+		                }
+		                if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
+		                    resolve(parsedResponse);
+		                } else {
+		                    reject("Problem authenticating with TidyHQ");
+		                }
+	           	 	}
+				});
+
+				browserRef.addEventListener("exit", function(event) {
+	            	reject("The Facebook sign in flow was canceled");
+	        	});
+			} else {
+				console.log("Not mobile app..");
+				location.replace(apiURL);
+				resolve("Window URL change.");
+			}
+
+			//window.href=apiURL;
+
+			console.log(browserRef);
+	    });
+		
+	}
+
+	private APIgetRequest(req:string, data_callback:Function){
+		var that = this;
+		//var header:any = { 'Authorization' : 'Bearer ' + that.access_token};
+		
+		this.http.get('https://api.tidyhq.com/v1/' + req + "?access_token=" + that.access_token)
+			.subscribe(data => {
+				data_callback(data);
+			}, error => {
+				console.log(JSON.stringify(error.json()));
+			}
+		);
+	}
+
+	public getContacts(data_callback:Function){
+		this.APIgetRequest("contacts", data_callback);
+	}
+}
